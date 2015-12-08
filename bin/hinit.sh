654984e1f5407cb77704b6707e0455b33371af74
Han Xu <genghisu@gmail.com>
2015-12-08 18:31:46 -0500

    Updating build file.

--- bin/hinit.sh
+++ bin/hinit.sh
@@ -4,22 +4,19 @@ USER=hxu
 THOME=/tmp/$USER
 HOME=/home/$USER
 
+#### Ubuntu Package Installation
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python3 git vim build-essential
+
 #### Main Setup
 sudo mkdir $THOME
 sudo chown $USER $THOME
 git clone git@github.com:genghisu/home.git       $THOME
-git clone git@github.com:genghisu/dotfiles.git   $THOME/.dotfiles
-git clone git@github.com:robbyrussell/oh-my-zsh.git  $THOME/.oh-my-zsh
+git clone git@github.com:genghisu/oh-my-zsh.git  $THOME/.oh-my-zsh
 
-git clone git://github.com/sstephenson/rbenv.git  $THOME/.rbenv
-mkdir -p $THOME/.rbenv/plugins
-git clone git://github.com/sstephenson/ruby-build.git $THOME/.rbenv/plugins/ruby-build
-
-cd $THOME/.dotfiles/
-git submodule update --init --recursive >/dev/null
-
-#rsync -avtz --delete $THOME/ $HOME/
-#rm -rf $THOME
+rsync -avtz --delete $THOME/ $HOME/
+rm -rf $THOME
 
 # symlinks
 chown -Rh $USER $HOME
